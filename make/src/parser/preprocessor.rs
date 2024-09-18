3d8629c0ba57f5636e1a01ce97edb578d90482fb
Ivan Dereviankin <ukrustacean@gmail.com>
2024-09-18 08:50:11 +0200

    [make] immediate and delayed macros support

--- make/src/parser/preprocessor.rs
+++ make/src/parser/preprocessor.rs
@@ -1,5 +1,7 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::iter::Peekable;
+use crate::parser::parse::Parse;
+use crate::parser::SyntaxKind;
 
 struct Preprocessor;
 
@@ -44,72 +46,98 @@ fn generate_macro_table(source: &str) -> HashMap<String, String> {
     let mut macro_table = HashMap::<String, String>::new();
 
     for def in macro_defs {
+        let mut immediate = false;
         let mut text = def.chars().peekable();
 
         let macro_name = get_ident(&mut text);
         skip_blank(&mut text);
-        let Some('=') = text.next() else { panic!("Expected `equals` sign after the macro name `{}`", macro_name) };
+        let Some(symbol) = text.next() else { continue };
+        match symbol { 
+            '=' => {}
+            ':' => {
+                let Some('=') = text.next() else { panic!("Expected `=` after `:` in macro definition") };
+                immediate = true;
+            }
+            c => panic!("Unexpected symbol `{}` in macro definition", c)
+        };
         skip_blank(&mut text);
         let macro_body = take_till_newline(&mut text);
 
-        macro_table.insert(macro_name, macro_body);
+        macro_table.insert(macro_name, if immediate { substitute(&macro_body, &macro_table).0 } else { macro_body });
     }
 
     macro_table
 }
 
-pub fn preprocess(source: &str) -> String {
-    let mut source = source.to_string();
-    
-    loop {
-        let mut substitutions = 0;
-        let mut result = String::with_capacity(source.len());
-        let table = generate_macro_table(&source);
+fn substitute(source: &str, table: &HashMap<String, String>) -> (String, u32) {
+    let mut substitutions = 0;
+    let mut result = String::with_capacity(source.len());
 
-        let mut letters = source.chars().peekable();
-        loop {
-            let Some(letter) = letters.next() else { break };
-            if letter != '$' {
-                result.push(letter);
-                continue;
-            }
-
-            // TODO: Make proper error handling
-            let Some(letter) = letters.next() else { panic!("Unexpected EOF after `$` symbol"); };
-            match letter {
-                // Internal macros - we leave them "as is"
-                // yet as they will be dealt with in the
-                // parsing stage with more context available  
-                c @ ('$' | '@' | '%' | '?' | '<' | '*') => {
-                    result.push('$');
-                    result.push(c);
-                    continue;
-                }
-                c if suitable_ident(&c) => {
-                    // TODO: Make proper error handling
-                    let Some(macro_body) = table.get(&c.to_string()) else { panic!("Undefined macro `{}`", c) };
-                    result.push_str(macro_body);
-                    substitutions += 1;
-                    continue;
-                }
-                '(' | '{' => {
-                    skip_blank(&mut letters);
-                    let macro_name = get_ident(&mut letters);
-                    skip_blank(&mut letters);
-                    let Some(finilizer) = letters.next() else { panic!("Unexpected EOF at the end of macro expansion") };
-                    if !matches!(finilizer, ')' | '}') { panic!("Unexpected `{}` at the end of macro expansion", finilizer) }
-
-                    let Some(macro_body) = table.get(&macro_name) else { panic!("Undefined macro `{}`", macro_name) };
-                    result.push_str(macro_body);
-                    substitutions += 1;
-
-                    continue;
-                }
-                // TODO: Make proper error handling
-                c => { panic!("Unexpected `{}` after `$` symbol", c); }
-            }
+    let mut letters = source.chars().peekable();
+    while let Some(letter) = letters.next() {
+        if letter != '$' {
+            result.push(letter);
+            continue;
         }
 
-        if substitutions == 0 { break result; } else { source = result; }
+        // TODO: Make proper error handling
+        let Some(letter) = letters.next() else { panic!("Unexpected EOF after `$` symbol"); };
+        match letter {
+            // Internal macros - we leave them "as is"
+            // yet as they will be dealt with in the
+            // parsing stage with more context available  
+            c @ ('$' | '@' | '%' | '?' | '<' | '*') => {
+                result.push('$');
+                result.push(c);
+                continue;
+            }
+            c if suitable_ident(&c) => {
+                // TODO: Make proper error handling
+                let Some(macro_body) = table.get(&c.to_string()) else { panic!("Undefined macro `{}`", c) };
+                result.push_str(macro_body);
+                substitutions += 1;
+                continue;
+            }
+            '(' | '{' => {
+                skip_blank(&mut letters);
+                let macro_name = get_ident(&mut letters);
+                skip_blank(&mut letters);
+                let Some(finilizer) = letters.next() else { panic!("Unexpected EOF at the end of macro expansion") };
+                if !matches!(finilizer, ')' | '}') { panic!("Unexpected `{}` at the end of macro expansion", finilizer) }
+
+                let Some(macro_body) = table.get(&macro_name) else { panic!("Undefined macro `{}`", macro_name) };
+                result.push_str(macro_body);
+                substitutions += 1;
+
+                continue;
+            }
+            // TODO: Make proper error handling
+            c => { panic!("Unexpected `{}` after `$` symbol", c); }
+        }
     }
+
+    (result, substitutions)
+}
+
+pub fn preprocess(source: &str) -> String {
+    let mut source = source.to_string();
+    let table = generate_macro_table(&source);
+    
+    
+    loop {
+        let (result, substitutions) = substitute(&source, &table);
+        if substitutions == 0 { break result } else { source = result }
+    }
+}
+
+pub fn postprocess(mut parse: Parse) -> Parse {
+    let mut queue = VecDeque::new();
+    queue.push_back(parse.syntax());
+    
+    while let Some(node) = queue.pop_front() {
+        println!("{:?}: {}", node.kind(), node.text());
+        node.children().for_each(|child| queue.push_back(child));
+    }
+    
+    parse
 }
