55f7714c9e09d6889963b92e2905050f4123f342
Ivan "ukrustacean" Dereviankin <ukrustacean@gmail.com>
2024-10-21 08:41:23 +0200

    [make] `-l` and `-r`

--- cron/crontab.rs
+++ cron/crontab.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use std::error::Error;
+use std::fs;
+use std::io::Read;
 
 #[derive(Parser)]
 struct CronArgs {
@@ -12,11 +15,44 @@ struct CronArgs {
     file: Option<String>,
 }
 
+// TODO: implement better error handling
+fn list_crontab() -> Result<String, Box<dyn Error>> {
+    let logname = std::env::var("LOGNAME")?;
+    let file = format!("/var/spool/cron/{logname}");
+    fs::read_to_string(&file).map_err(|x| Box::new(x) as Box<dyn Error>)
+}
+
+fn remove_crontab() -> Result<(), Box<dyn Error>> {
+    let logname = std::env::var("LOGNAME")?;
+    let path = format!("/var/spool/cron/{logname}");
+    fs::remove_file(&path).map_err(|x| Box::new(x) as Box<dyn Error>)
+}
+
 fn main() {
     let args = CronArgs::parse();
 
+
     if args.edit { println!("edit") }
-    if args.list { println!("list") }
-    if args.remove { println!("remove") }
+
+    if args.list {
+        match list_crontab() {
+            Ok(content) => println!("{}", content),
+            Err(_) => {
+                println!("No crontab file");
+                std::process::exit(1);
+            }
+        }
+    }
+
+    if args.remove {
+        match remove_crontab() {
+            Ok(()) => println!("Removed crontab file"),
+            Err(_) => {
+                println!("No crontab file");
+                std::process::exit(1);
+            },
+        }
+    }
+
     if let Some(file) = args.file { println!("FILE = {file}") }
 }
