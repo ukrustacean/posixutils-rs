a2282f3b2d791cdc8412ce575fb365da022f99e9
Ivan "ukrustacean" Dereviankin <ukrustacean@gmail.com>
2024-10-21 09:07:30 +0200

    [make] daemon

---
+++ cron/crond.rs
@@ -0,0 +1,23 @@
+use std::error::Error;
+use std::fs;
+
+struct CronJob {
+    date: String,
+}
+
+fn parse_cronfile(username: &str) -> Result<Vec<CronJob>, Box<dyn Error>> {
+    let file = format!("/var/spool/cron/{logname}");
+    fs::read_to_string(&file)?;
+    Ok(vec![])
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let logname = std::env::var("LOGNAME")?;
+    let pid = unsafe { libc::fork() };
+    if pid > 0 { return Ok(()) }
+    
+    unsafe { libc::setsid() };
+    
+    std::thread::sleep(std::time::Duration::from_secs(10));
+    Ok(())
+}
